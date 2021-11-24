f6af80e34c2fcb3943c04ac0ff0c847618c08cf1
beautnatakorn <nbaramee@cmkl.ac.th>
2021-11-24 19:02:08 +0700

    add sound

---
+++ HappyFiveFriends/Assets/Scripts/Sound.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound : MonoBehaviour
+{
+    public AudioClip mkBG;
+    public AudioClip endMKsound;
+    public AudioClip gameOverSound;
+    public AudioClip objectCollision;
+    public AudioClip brokenPlate;
+    // merge
+    public AudioClip winSoundYeah;
+    public AudioClip winSoundWow;
+    // merge sounds
+    public AudioClip shakingTowerOne;
+    public AudioClip shakingTowerTwo;
+    // loopable
+    public AudioClip timeIsUp;
+    public AudioClip clickButton;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
