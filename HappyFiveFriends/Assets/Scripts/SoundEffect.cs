e4b5c5a1f0ab37b952b88d815766c0a7b665e649
beautnatakorn <nbaramee@cmkl.ac.th>
2021-11-26 02:23:58 +0700

    beaut update

--- HappyFiveFriends/Assets/Scripts/SoundEffect.cs
+++ HappyFiveFriends/Assets/Scripts/SoundEffect.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class SoundEffect : MonoBehaviour
 {
+    //Stackingsound will play when get good tray
+    public AudioSource stackingSound;
+    //Nope will play when get the bad tray
+    public AudioSource nopeSound;
+    //Brokenplate will play when all trays fall on the ground
+    public AudioSource brokenPlate;
+    //ShakingTowers will play when trays almost fall
+    public AudioSource shakingTower;
+    //GameOver will play when all plates fall down
+    public AudioSource gameOver;
+    //KidsYeah will play when you win
+    public AudioSource kidsYeah;
+    //TimerSound will play when time less than 10 seconds
+    public AudioSource timerSound;
+
     // Start is called before the first frame update
     void Start()
     {
