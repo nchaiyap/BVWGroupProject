18dfa406775f01da5355a866277eb7c44d399585
nchaiyap <94219277+nchaiyap@users.noreply.github.com>
2021-11-26 20:55:00 +0700

    Point Cloud Edit

---
+++ SoundTestGame/Assets/Scripts/SoundTest.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundTest : MonoBehaviour
+{
+    public AudioSource mkBG;
+    public AudioSource endMKsound;
+    public AudioSource gameOverSound;
+    public AudioSource objectCollision;
+    public AudioSource brokenPlate;
+    public AudioSource winSoundYeah;
+    public AudioSource winSoundWow;
+    public AudioSource shakingTowerOne;
+    public AudioSource shakingTowerTwo;
+    public AudioSource timeIsUp;
+    public AudioSource clickButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
